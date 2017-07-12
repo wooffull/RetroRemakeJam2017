746c24d70ffd3e0ff906efaa1f0a10da5a4710e5
Rafael Eduardo Lopez Montilla <rafael.lopez.leaf@gmail.com>
2017-07-12 18:52:08 -0400

    made rough draft sprites of pit, also uploaded original sprites for reference

---
+++ Assets/jump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// MonoBehaviours don't normally have constructors
+public class jump : MonoBehaviour {
+
+	public int jumpspeed = 500;
+
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool jumpIsDown = Input.GetButtonDown("Jump");
+		float horizontalAxis = Input.GetAxis("Horizontal");
+
+		if (jumpIsDown) {
+			rigidBody.velocity += Vector2.up * jumpspeed * Time.deltaTime;
+		}
+			
+		Debug.Log (jumpIsDown);
+	}
+}
