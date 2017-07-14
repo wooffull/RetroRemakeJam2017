7ab6f42dfb4c8119e051386d105479e03a26385c
wooffull <hfp1355@rit.edu>
2017-07-14 16:09:14 -0400

    Destroy objects that fall too far below the camera, and restart the level when Pit falls

---
+++ Assets/DestroyWhenBelowCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyWhenBelowCamera : MonoBehaviour
+{
+    // Instead of destroying things when they fall below the camera, allow an offset so
+    // things can still be allowed a little below
+    public float bottomOffset = 1.28f;
+
+    private new Camera camera;
+    private new Collider2D collider;
+    private Vector3 viewportBottomLeft;
+    private Vector3 viewportTopRight;
+
+    // Use this for initialization
+    void Start ()
+    {
+        camera = Camera.main;
+        viewportBottomLeft = camera.ViewportToWorldPoint(new Vector3(0, 0, transform.position.z));
+        viewportTopRight = camera.ViewportToWorldPoint(new Vector3(1, 1, transform.position.z));
+        collider = GetComponent<Collider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (collider.bounds.max.y < camera.transform.position.y + viewportBottomLeft.y - bottomOffset)
+        {
+            Destroy(gameObject);
+
+            if (gameObject.tag == "Player")
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+	}
+}
