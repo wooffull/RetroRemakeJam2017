691dbc38e95ef5253e8ca55f51bde23c64eb59c5
David <dxl4041@rit.edu>
2017-07-17 17:32:14 -0700

    Added high score tracker(but maybe broken?) and edited title screen

--- Assets/TitleScreen.cs
+++ Assets/TitleScreen.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScreen : MonoBehaviour {
-	// Update is called once per frame
-	void Update () {
-        if (Input.anyKeyDown)
+
+    private int highScore;
+    private Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("High Score");
+        text = GameObject.Find("High Score").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        text.text = "High Score: " + highScore.ToString().PadLeft(3, '0');
+
+        if (Input.GetKeyDown("q"))
+        {
+            Application.Quit();
+        }
+        else if (Input.anyKeyDown)
         {
             SceneManager.LoadScene("GameScene");
         }
