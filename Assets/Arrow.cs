045580fa0c5ac32712e2658839618f4c2614851b
wooffull <hfp1355@rit.edu>
2017-07-19 21:16:00 -0400

    Fix bug where arrow can shoot enemies through blocks

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -15,19 +15,60 @@ public class Arrow : MonoBehaviour {
 
     [HideInInspector]
     public int damage = 1;
-
-    private bool arrowDisabled = false;
+    
     private float lifeTime = 0.0f;
     private AudioManager audioManager;
+    private List<GameObject> hitObjects;
 
     // Use this for initialization
     void Start ()
     {
         audioManager = GameObject.Find("Audio").GetComponent<AudioManager>();
+        hitObjects = new List<GameObject>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (damage <= 0)
+        {
+            return;
+        }
+
+        // If this arrow has hit objects, find the nearest and handle it accordingly
+        if (hitObjects.Count > 0)
+        {
+            GameObject nearest = hitObjects[0];
+            float distToNearest = Vector2.SqrMagnitude(nearest.transform.position - transform.position);
+
+            for (int i = 1; i < hitObjects.Count; i++)
+            {
+                GameObject cur = hitObjects[i];
+                float distToCur = Vector2.SqrMagnitude(cur.transform.position - transform.position);
+                
+                if (distToCur < distToNearest)
+                {
+                    distToNearest = distToCur;
+                    nearest = cur;
+                }
+            }
+
+            // Damage the enemy and play a sound
+            if (nearest.tag == "Enemy")
+            {
+                if (damage > 0)
+                {
+                    Stats stats = nearest.GetComponent<Stats>();
+                    stats.health -= damage;
+                    damage = 0;
+
+                    audioManager.PlayTakeDamageSound();
+                }
+            }
+
+            Destroy(gameObject);
+            return;
+        }
+
         Vector3 velocity = direction * speed * Time.deltaTime;
         transform.position += velocity;
 
@@ -41,29 +82,9 @@ public class Arrow : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D c)
     {
-        // Ensures the arrow doesn't affect enemies through bocks
-        if(c.gameObject.tag == "Block")
-        {
-            arrowDisabled = true;
-        }
-
-        // Damage the enemy and play a sound
-        if (c.gameObject.tag == "Enemy" && !arrowDisabled)
-        {
-            if (damage > 0)
-            {
-                Stats stats = c.gameObject.GetComponent<Stats>();
-                stats.health -= damage;
-                damage = 0;
-
-                audioManager.PlayTakeDamageSound();
-            }
-        }
-
         if (c.gameObject.tag == "Enemy" || c.gameObject.tag == "Block")
         {
-            damage = 0;
-            Destroy(gameObject);
+            hitObjects.Add(c.gameObject);
         }
     }
 }
